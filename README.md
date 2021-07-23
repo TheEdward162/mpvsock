249c60077500e947a2388155dcf0b92e3a66c227
TheEdward162 <TheEdward162@gmail.com>
2021-07-24 00:30:49 +0200

    all event names

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # mpvsock
 
 mpv socket ipc client with MIT/Apache-2.0 license.
+
+## cli_app
+
+Try with (to spawn a child mpv instance):
+
+```
+cargo run --bin mpv-client -- --verbosity Trace --spawn-client interactive
+```
+
+or with (to connect to an existing `--input-ipc-server=/path/to/socket` instance):
+
+```
+cargo run --bin mpv-client -- --verbosity Trace --connect /path/to/socket interactive
+```
--- src/command/response.rs
+++ src/command/response.rs
@@ -6,18 +6,56 @@ use serde::Deserialize;
 /// { "event": "idle" }
 /// { "event": "property-change", id: number, name: "property-name", data: "property-value" }
 /// ```
+///
+/// See https://mpv.io/manual/stable/#list-of-events.
 #[derive(Debug, Deserialize)]
 #[serde(tag = "event")]
 pub enum MpvResponseEvent {
-	#[serde(rename = "idle")]
-	Idle,
 	#[serde(rename = "property-change")]
 	PropertyChange {
 		/// Id of the observer.
 		id: i64,
 		name: MpvResponseEventPropertyName,
 		data: serde_json::Value
-	}
+	},
+	#[serde(rename = "log-message")]
+	LogMessage {}, // TOOD
+	// media
+	#[serde(rename = "start-file")]
+	StartFile {
+		playlist_entry_id: i64
+	},
+	#[serde(rename = "end-file")]
+	EndFile {}, // TODO
+	#[serde(rename = "file-loaded")]
+	FileLoaded,
+	#[serde(rename = "seek")]
+	Seek,
+	#[serde(rename = "playback-restart")]
+	PlaybackRestart,
+	#[serde(rename = "shutdown")]
+	Shutdown,
+	#[serde(rename = "audio-reconfig")]
+	AudioReconfig,
+	#[serde(rename = "video-reconfig")]
+	VideoReconfig,
+	// deprecated
+	#[serde(rename = "tracks-changed")]
+	TracksChanged,
+	#[serde(rename = "track-switched")]
+	TrackSwitched,
+	#[serde(rename = "pause")]
+	Pause,
+	#[serde(rename = "unpause")]
+	Unpause,
+	#[serde(rename = "metadata-update")]
+	MetadataUpdate,
+	#[serde(rename = "idle")]
+	Idle,
+	#[serde(rename = "tick")]
+	Tick,
+	#[serde(rename = "chapter-change")]
+	ChapterChange
 }
 
 #[derive(Debug, Deserialize)]
