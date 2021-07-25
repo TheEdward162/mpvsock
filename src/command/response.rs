a7719540f8bd7eebbc8be2a6e6d4914fe8fd3ce3
TheEdward162 <TheEdward162@gmail.com>
2021-07-25 19:41:16 +0200

    update response models

--- src/command/response.rs
+++ src/command/response.rs
@@ -1,4 +1,4 @@
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer, de::IntoDeserializer};
 
 /// Event model:
 ///
@@ -15,7 +15,9 @@ pub enum MpvResponseEvent {
 	PropertyChange {
 		/// Id of the observer.
 		id: i64,
+		#[serde(deserialize_with = "MpvResponseEventPropertyName::deserialize_with_unknown")]
 		name: MpvResponseEventPropertyName,
+		#[serde(default)]
 		data: serde_json::Value
 	},
 	#[serde(rename = "log-message")]
@@ -39,29 +41,53 @@ pub enum MpvResponseEvent {
 	AudioReconfig,
 	#[serde(rename = "video-reconfig")]
 	VideoReconfig,
+
 	// deprecated
-	#[serde(rename = "tracks-changed")]
-	TracksChanged,
-	#[serde(rename = "track-switched")]
-	TrackSwitched,
-	#[serde(rename = "pause")]
-	Pause,
-	#[serde(rename = "unpause")]
-	Unpause,
-	#[serde(rename = "metadata-update")]
-	MetadataUpdate,
-	#[serde(rename = "idle")]
-	Idle,
-	#[serde(rename = "tick")]
-	Tick,
-	#[serde(rename = "chapter-change")]
-	ChapterChange
+	// #[serde(rename = "tracks-changed")]
+	// TracksChanged,
+	// #[serde(rename = "track-switched")]
+	// TrackSwitched,
+	// #[serde(rename = "pause")]
+	// Pause,
+	// #[serde(rename = "unpause")]
+	// Unpause,
+	// #[serde(rename = "metadata-update")]
+	// MetadataUpdate,
+	// #[serde(rename = "idle")]
+	// Idle,
+	// #[serde(rename = "tick")]
+	// Tick,
+	// #[serde(rename = "chapter-change")]
+	// ChapterChange
+
+	// unknown
+	#[serde(other)]
+	Unknown
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+
 pub enum MpvResponseEventPropertyName {
-	#[serde(rename = "volume")]
-	Volume
+	Volume,
+	Filename,
+	#[serde(rename = "filename/no-ext")]
+	FilenameNoExt,
+	// unknown
+	#[serde(skip_deserializing)]
+	Unknown(String)
+}
+impl MpvResponseEventPropertyName {
+	pub fn deserialize_with_unknown<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+		let string = String::deserialize(deserializer)?;
+
+		match Self::deserialize(
+			IntoDeserializer::<'de, D::Error>::into_deserializer(string.as_str())
+		) {
+			Ok(value) => Ok(value),
+			Err(_) => Ok(Self::Unknown(string))
+		}
+	}
 }
 
 /// Result model:
@@ -82,7 +108,9 @@ pub enum MpvResponseResultError {
 	#[serde(rename = "success")]
 	Success,
 	#[serde(rename = "invalid parameter")]
-	InvalidParameter
+	InvalidParameter,
+	#[serde(rename = "property unavailable")]
+	PropertUnavailable
 }
 
 /// Either a mpv event or a mpv result.
@@ -91,4 +119,75 @@ pub enum MpvResponseResultError {
 pub enum MpvResponse {
 	Event(MpvResponseEvent),
 	Result(MpvResponseResult)
+}
+
+#[cfg(test)]
+mod test {
+	use super::{MpvResponseEvent, MpvResponseEventPropertyName};
+
+	#[test]
+	fn test_mpv_response_event_property_change() {
+		let json = serde_json::json!(
+			{
+				"event": "property-change",
+				"id": 1,
+				"name": "filename"
+			}
+		);
+
+		let response: MpvResponseEvent = serde_json::from_value(json).unwrap();
+
+		match response {
+			MpvResponseEvent::PropertyChange {
+				id: 1,
+				name: MpvResponseEventPropertyName::Filename,
+				data: serde_json::Value::Null
+			} => (),
+			me => panic!("Expected MpvResponseEvent::PropertyChange {{ id: 1, name: Filename, data: null }} but found {:?}", me)
+		}
+	}
+
+	#[test]
+	fn test_mpv_response_event_property_change_name_unknown_parse() {
+		let json = serde_json::json!(
+			{
+				"event": "property-change",
+				"id": 1,
+				"name": "whatever-unknown-thing"
+			}
+		);
+
+		let response: MpvResponseEvent = serde_json::from_value(json).unwrap();
+
+		match response {
+			MpvResponseEvent::PropertyChange {
+				id: 1,
+				name: MpvResponseEventPropertyName::Unknown(ref name),
+				data: serde_json::Value::Null
+			} if name == "whatever-unknown-thing" => (),
+			me => panic!("Expected MpvResponseEvent::PropertyChange {{ id: 1, name: Unknown(\"whatever-unknown-thing\"), data: null }} but found {:?}", me)
+		}
+	}
+
+	#[test]
+	fn test_mpv_response_event_unknown_parse() {
+		let json = serde_json::json!(
+			{
+				"event": "idle"
+			}
+		);
+
+		let response: MpvResponseEvent = serde_json::from_value(json).unwrap();
+
+		// TODO: Is this feasible?
+		// match response {
+		// 	MpvResponseEvent::Unknown(ref tag) if tag == "idle" => (),
+		// 	me => panic!("Expected MpvResponseEvent::Unknown(\"idle\") but found {:?}", me)
+		// }
+
+		match response {
+			MpvResponseEvent::Unknown => (),
+			me => panic!("Expected MpvResponseEvent::Unknown() but found {:?}", me)
+		}
+	}
 }
